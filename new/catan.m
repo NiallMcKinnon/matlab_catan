962c3f2d6ba8901be22f58b23d5b51de44d59042
Niall Richard McKinnon <nrm3009@uncw.edu>
2022-08-20 17:11:32 -0400

    Improved map generation

--- new/catan.m
+++ new/catan.m
@@ -4,30 +4,23 @@ rng shuffle;
 
 % Create a UI window:
 windowWidth = 800;
-window = uifigure(Name="Catan", Units="pixels", Position=[0, 0, windowWidth, windowWidth]);
+window = uifigure(Name="Catan", Units="pixels", Position=[100, 100, windowWidth, windowWidth]);
 
 % Create panel for the game board:
 boardWidth = windowWidth / 2;
 board = uipanel(window, Units="pixels", Position=[0, boardWidth, boardWidth, boardWidth], BackgroundColor=[0 191 255]/255);
 
 % Create a plot for the map:
-map = uiaxes(Parent=board);
+map = uiaxes(Parent=board, Position=[-8, 0, boardWidth, boardWidth]);
 
-% radius = 5;
-% xSpacing = 2 * (sqrt(3)/2 * radius);
-% ySpacing = radius + radius / 2;
-
-% radiusOcean = 25;
-% theta = linspace(0, 360, 7);
-% xCircle = radiusOcean * cosd(theta) + 3 * xSpacing;
-% yCircle = radiusOcean * sind(theta);
-% fill(xCircle, yCircle, [255 0 0]/255, Parent=map);
-
+% Adjust plot settings:
 map.Toolbar.Visible = "off";
+disableDefaultInteractivity(map); % This line may need to be removed
 axis(map, "off");
 hold(map, "on");
 axis(map, "equal");
 
+% map.Position = [0, 0, boardWidth, boardWidth];
 [matrixMap, xNumberMap, yNumberMap, diceMap] = plotMap(map);
 
 % canBuild arrays keep track of where things can be built:
@@ -39,3 +32,4 @@ victoryPoints = zeros(1, 2);
 
 % resourceScoreboard keeps track of the players' resources:
 resourceScoreboard = zeros(2, 6);
+
